40d6fa85f2f962f1a79481241012ae6c099f262c
Ahmet Akkuş <47339367+akacdrom@users.noreply.github.com>
2022-03-30 14:18:09 +0200

    Add files via upload
    Different approach used by "Parallel.For()" method.

--- Program.cs
+++ Program.cs
@@ -1,124 +1,111 @@
 ﻿using System.Diagnostics;
-namespace Program
-{
-    class Program {         
+    class Program {
+        int [,] firstArray;
+        int [,] secondArray;
+
         static void Main(string[] args)
         {
-            int matrix1Row = 10000;
-            int matrix1Colm= 10000;
-            int matrix2Row = 10000;
-            int matrix2Colm= 10000;
+            Program program = new Program();
+            program.getArrays();
+        }
 
-            Stopwatch sw = new Stopwatch();
+    private void getArrays(){
+        Program program = new Program();
+        program.createFirstArray(firstArray);
+        program.createSecondArray(secondArray);
 
-            Random rnd = new Random();
-            Console.WriteLine("First Matrix is being created...");
-            int[,] matrix1 = new int[matrix1Row,matrix1Colm];
-            for(int i=0;i<matrix1Row;i++)
+        serialMatrix(program.getFirstArray(),program.getSecondArray());
+        parallelMatrix(program.getFirstArray(),program.getSecondArray());
+    }
+
+    private  void createFirstArray(int[,] firstArray){
+            Random random = new Random();
+            firstArray = new int[1000,1000];
+            for(int i=0;i<1000;i++)
             {
-                for(int j=0;j<matrix1Colm;j++)        
+                for(int j=0;j<1000;j++)        
                 {
-                    matrix1[i, j]= rnd.Next();
-                    //Console.WriteLine("[{0}, {1}] = {2}", i, j, m1[i,j]);
+                    firstArray[i, j]= random.Next(1,10);
                 }
             }
-            Console.WriteLine("First Matrix is created!\n");
-
-            Console.WriteLine("Second Matrix is being created...");
-            int[,] matrix2 = new int[matrix2Row,matrix2Colm];
-            for(int i=0;i<matrix2Row;i++)
+            this.firstArray = firstArray;
+    }
+    private int[,] getFirstArray(){
+        return this.firstArray;
+    }
+    private void createSecondArray(int[,] secondArray){
+            Random random = new Random();
+            secondArray = new int[1000,1000];
+            for(int i=0;i<1000;i++)
             {
-                for(int j=0;j<matrix2Colm;j++)        
+                for(int j=0;j<1000;j++)        
                 {
-                    matrix2[i,j]= rnd.Next();
-                    //Console.WriteLine("[{0}, {1}] = {2}", i, j, m2[i,j]);
+                    secondArray[i,j]= random.Next(1,10);
                 }
             }
-            Console.WriteLine("Second Matrix is created!");
+            this.secondArray = secondArray;
+    }
+    private int[,] getSecondArray(){
+        return this.secondArray;
+    }
+    private static int[,] serialMatrix(int[,] firstArray, int[,] secondArray)
+    {
+            Stopwatch stopwatch = new Stopwatch();
 
-            int s = 0;
-            int[,] newSequentialMatrix = new int[10000,10000];
-            Console.WriteLine();
-            Console.WriteLine("Sequential Matrix multiplication started...");
+            int temp = 0;
+            int[,] serialArray = new int[1000,1000];
+            Console.WriteLine("Serial calculations happening");
 
-            sw.Start();
-            for (int i = 0; i < 10000; i++)
+            stopwatch.Start();
+            for (int i = 0; i < 1000; i++)
             {
-                for (int j = 0; j < 10000; j++)
+                for (int j = 0; j < 1000; j++)
                 {
-                    for (int k = 0; k < 10000; k++)
+                    for (int k = 0; k < 1000; k++)
                     {
-                        s = s + matrix1[i,k]*matrix2[k,j];
+                        temp += firstArray[k,i]*secondArray[k,j];
                     }
-                    newSequentialMatrix[i,j] = s;
-                    s = 0;
+                    serialArray[i,j] = temp;
+                    temp = 0;
                 }
             }
-            sw.Stop();
-            Console.WriteLine("Sequential Matrix multiplication is finished -- "+ sw.ElapsedMilliseconds + " ms.");
-            Console.WriteLine("Last index of matrix is: "+newSequentialMatrix[9999,9999]);
-            
-            //  for (int i = 0; i < m1Row; i++){
-            //      for (int j = 0; j < m2Colm; j++)
-            //      {
-            //          Console.Write(newMatrix[i,j]+ " ");
-            //      }
-            //      Console.WriteLine();
-            //  }
-            //  Console.WriteLine("Final matrix is printed");
-        
+            stopwatch.Stop();
+            Console.WriteLine("serial matrix: "+ stopwatch.ElapsedMilliseconds + " ms.");
+        return serialArray;
+    }
 
-        ParallelOptions parallelOptions = new ParallelOptions{
-            MaxDegreeOfParallelism = 4
-        };
+    private static int[,] parallelMatrix(int[,] firstArray, int[,] secondArray)
+    {
+        Stopwatch stopwatch = new Stopwatch();
 
         Console.WriteLine();
-        Console.WriteLine("Parallel Matrix multiplication started...");
-        int[,] newParallelMatrix = new int[10000,10000];
-        int[] subtotalls = new int[8];
+        Console.WriteLine("Parallel calculations happening");
 
-        sw.Reset();
-        sw.Start();
-        Parallel.Invoke(
-            parallelOptions,
-            () => subtotalls[0] = CalculatePart(1, matrix1Row, matrix1, matrix2, newParallelMatrix),
-            () => subtotalls[1] = CalculatePart(2, matrix1Row, matrix1, matrix2, newParallelMatrix),
-            () => subtotalls[2] = CalculatePart(3, matrix1Row, matrix1, matrix2, newParallelMatrix),
-            () => subtotalls[3] = CalculatePart(4, matrix1Row, matrix1, matrix2, newParallelMatrix),
-            () => subtotalls[4] = CalculatePart(5, matrix1Row, matrix1, matrix2, newParallelMatrix),
-            () => subtotalls[5] = CalculatePart(6, matrix1Row, matrix1, matrix2, newParallelMatrix),
-            () => subtotalls[6] = CalculatePart(7, matrix1Row, matrix1, matrix2, newParallelMatrix),
-            () => subtotalls[7] = CalculatePart(8, matrix1Row, matrix1, matrix2, newParallelMatrix)
-        );
-        sw.Stop();
-        Console.WriteLine("Parallel Matrix multiplication is finished! -- " + sw.ElapsedMilliseconds + " ms.");
+        int[,] parallelArray = new int[1000,1000];
 
-        int lastIndex = 0;
-         foreach (var subtotall in subtotalls)
-         {
-            lastIndex = subtotall;
-         }
-         Console.WriteLine("Last index of matrix is: "+ lastIndex);
-    }
-        static int CalculatePart(int n, int sizeOfMatrix, int[,] matrix1, int[,] matrix2, int[,]newParallelMatrix){
-            //string value = "";
-            int s = 0;
+        var options = new ParallelOptions(){
+            MaxDegreeOfParallelism = 4
+        };
+        stopwatch.Start();
 
-            Console.WriteLine("Thread Number: " + Thread.CurrentThread.ManagedThreadId);
-            for (int i = (n - 1) * (sizeOfMatrix / 8); i < n * (sizeOfMatrix / 8); i++){
-                
-                    for (int j = 0; j < 10000; j++)
+        //I think using lock would be unnecessary so I didn't use.
+        object lck = new object();
+        Parallel.For(0, 1000, options, count => 
+        {   
+            int temp = 0;
+            int i = count;
+                    for (int j = 0; j < 1000; j++)
                     {
-                        for (int k = 0; k < 10000; k++)
+                        for (int k = 0; k < 1000; k++)
                         {
-                            s = s + matrix1[i,k]*matrix2[k,j];
+                           temp += firstArray[k,i]*secondArray[k,j];
                         }
-                        newParallelMatrix[i,j] = s;
-                        //value += s.ToString() + " ";
-                        s = 0;
-                    }
-            }
-            return newParallelMatrix[9999,9999];
-        }
+                        parallelArray[i,j] = temp;
+                        temp = 0;
+                    }            
+        });
+        stopwatch.Stop();
+        Console.WriteLine("parallel matrix: " + stopwatch.ElapsedMilliseconds + " ms.");
+        return parallelArray;
     }
 }
